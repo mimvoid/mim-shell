42b126b1169fd8716fe82d8a6f61e10f2d26f8e8
mimvoid <mimvoid@proton.me>
2025-07-12 17:30:45 -0400

    Adjust wallpaper item widget

--- widgets/wallpapers/item.tsx
+++ widgets/wallpapers/item.tsx
@@ -1,13 +1,18 @@
 import { Astal, Gtk } from "ags/gtk4";
-import { register } from "ags/gobject";
+import { property, register } from "ags/gobject";
 import Gio from "gi://Gio?version=2.0";
 
 import Wallpapers from "@services/wallpapers";
 
 @register({ GTypeName: "WallpaperItem" })
 export default class WallpaperItem extends Astal.Box {
-  #picture: Gtk.Picture;
-  #label: Gtk.Label;
+  @property(Gtk.Picture) picture = new Gtk.Picture({
+    heightRequest: 144,
+    widthRequest: 256,
+    contentFit: Gtk.ContentFit.COVER,
+  });
+
+  @property(Gtk.Label) label = new Gtk.Label({ cssClasses: ["filename"] });
 
   constructor() {
     super({
@@ -17,24 +22,17 @@ export default class WallpaperItem extends Astal.Box {
     });
     this.set_cursor_from_name("pointer");
 
-    this.#picture = new Gtk.Picture({
-      heightRequest: 144,
-      widthRequest: 256,
-      contentFit: Gtk.ContentFit.COVER,
-    });
-    this.#label = new Gtk.Label({ cssClasses: ["filename"] });
-
-    this.append(this.#picture);
-    this.append(this.#label);
+    this.append(this.picture);
+    this.append(this.label);
   }
 
   setFileInfo(fileInfo: Gio.FileInfo) {
     const displayName = fileInfo.get_display_name();
     const thumbnail = fileInfo.get_attribute_byte_string("thumbnail::path");
 
-    this.#picture.set_file(
+    this.picture.set_file(
       Gio.File.new_for_path(thumbnail || Wallpapers.directory + displayName),
     );
-    this.#label.set_text(displayName);
+    this.label.set_text(displayName);
   }
 }
